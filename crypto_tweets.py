7e28ad8a492e0920717d7679ed039281a07692b3
thomktz <thomas@kientz.net>
2021-05-30 16:21:29 +0200

    /

--- crypto_tweets.py
+++ crypto_tweets.py
@@ -2,5 +2,11 @@
 Fetches all tweets from certain account talking about a certain crypto
 """
 # %%
+import tweepy
 from read_api_keys import consumer_key, consumer_secret, access_key, access_secret
-# %%
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+ 
+api = tweepy.API(auth,wait_on_rate_limit=True)
+
