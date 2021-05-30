633f4e74775d7eee5c30ac0aa8d7db9d50605af4
thomktz <thomas@kientz.net>
2021-05-30 17:37:59 +0200

    oo

--- crypto_tweets.py
+++ crypto_tweets.py
@@ -3,10 +3,57 @@ Fetches all tweets from certain account talking about a certain crypto
 """
 # %%
 import tweepy
+import csv
 from read_api_keys import consumer_key, consumer_secret, access_key, access_secret
+import time
+from pandas import DataFrame
+import pandas as pd
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_key, access_secret)
- 
-api = tweepy.API(auth,wait_on_rate_limit=True)
+def create_tweets():
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    api = tweepy.API(auth)
+    userID = "elonmusk"
+    tweets = api.user_timeline(screen_name=userID, 
+                            count=1000,
+                            include_rts = False,
+                            tweet_mode = 'extended')
+    all_tweets = []
+    all_tweets.extend(tweets)
+    oldest_id = tweets[-1].id
+    try:
+        while True:
+            time.sleep(1.1)
+            tweets = api.user_timeline(screen_name=userID, 
+                                count=2000,
+                                include_rts = False,
+                                max_id = oldest_id - 1,
+                                tweet_mode = 'extended')
+            all_tweets.extend(tweets)
+            oldest_id = all_tweets[-1].id
+            print('N of tweets downloaded till now {}'.format(len(all_tweets)))
+    except KeyboardInterrupt:
+        pass
 
+
+    outtweets = [[tweet.created_at,
+                tweet.full_text.encode("utf-8").decode("utf-8")] 
+                for idx,tweet in enumerate(all_tweets)]
+    df = DataFrame(outtweets,columns=["created_at", "text"])
+    df.to_csv('%s_tweets.csv' % userID,index=False)
+    print(df.head(3))
+
+
+def keep_tweets(keywords, user):
+    keep = []
+    df = pd.read_csv(user + "_tweets.csv")
+    for key in keywords:
+        keep.append(df[df['text'].str.contains(key)])
+        
+    out = pd.concat(keep)
+
+    return out
+        
+        
+        
+    # %%
